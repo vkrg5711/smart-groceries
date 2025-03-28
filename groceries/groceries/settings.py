7e8a8125b767370c39f65f9ad5000f56006c6da4
swanakgoswami <business.swanak@gmail.com>
2025-03-29 04:28:34 +0530

    Templates DIR added

--- groceries/groceries/settings.py
+++ groceries/groceries/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'groceries.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'grocery' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
